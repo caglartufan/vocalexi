2076bba13e197a8ffb618a0008879acd0a175c70
caglartufan <caglartufan09@gmail.com>
2025-08-12 21:17:19 +0300

    fix: Fix Dockerfile and standalone next.js output.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+  // Recommended: this will reduce output
+  // Docker image size by 80%+
+  output: 'standalone',
+  // Optional: bring your own cache handler
+  // cacheHandler: path.resolve('./cache-handler.mjs'),
+  // cacheMaxMemorySize: 0, // Disable default in-memory caching
   images: {
     remotePatterns: [
       {
@@ -11,6 +17,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // Nginx will do gzip compression. We disable
+  // compression here so we can prevent buffering
+  // streaming responses
+  compress: false,
+  // Optional: override the default (1 year) `stale-while-revalidate`
+  // header time for static pages
+  // swrDelta: 3600 // seconds
 };
 
 const withNextIntl = createNextIntlPlugin();
